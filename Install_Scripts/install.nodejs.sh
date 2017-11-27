d5b8561d8a9011193a7c2c81a17c719d8df81a84
Mayccoll <Mayccoll@users.noreply.github.com>
2017-11-27 14:54:19 -0500

    Nodejs 9

--- Install_Scripts/install.nodejs.sh
+++ Install_Scripts/install.nodejs.sh
@@ -3,7 +3,7 @@
 # Using Ubuntu
 # https://github.com/nodesource/distributions
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt install -y nodejs
 
 mkdir ~/.npm-global
